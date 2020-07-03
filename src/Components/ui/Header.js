e005da5e548c0d5d7649aa317397b0e0b97b7ffb
Brian Bantigue <bignatue@gmail.com>
2020-07-02 22:28:07 -0400

    First Commit

---
+++ src/Components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../img/logo.png';
+
+const Header = () => {
+  return (
+    <div className="center">
+      <img className="my-5" src={logo} alt="" />
+    </div>
+  )
+}
+
+export default Header;
